a5e8307f29b62f13a1bc54ea1c296457b6f1241c
MonyaS <salabaymisha@gmail.com>
2022-02-03 22:54:53 +0200

    1

--- Tic_Tac_Toe/Menu.cpp
+++ Tic_Tac_Toe/Menu.cpp
@@ -345,15 +345,16 @@ void Drops() {
         {
             continue;
         }
-
         for (int now_use_element = 0; now_use_element < drops[now_use_drop].size(); now_use_element++){
             COORD tmp = drops[now_use_drop][now_use_element];
             if (tmp.X >= 96 && tmp.X < 116 && tmp.Y >= 10 && tmp.Y <= 36) { continue; }
             else if (tmp.X >= 82 && tmp.X <= 95 && tmp.Y >= 10 && tmp.Y < 21) { continue; }
             else if (tmp.X >= 115 && tmp.X <= 128 && tmp.Y >= 10 && tmp.Y < 21) { continue; }
             else{
-                SetConsoleCursorPosition(hout_menu, { drops[now_use_drop][min_index].X, drops[now_use_drop][min_index].Y-  6 });
-                cout << " ";
+                if (now_use_element+1==drops[now_use_drop].size()){
+                    SetConsoleCursorPosition(hout_menu, { drops[now_use_drop][min_index].X, drops[now_use_drop][min_index].Y-  6 });
+                    cout << " ";
+                }
                 SetConsoleCursorPosition(hout_menu, {drops[now_use_drop][now_use_element].X, drops[now_use_drop][now_use_element].Y});
                 if (now_use_element % 2 == 0) cout << "X";
                 else cout << "O";
